187c88ac777d88bb8b3c88a822ba20a4f8af541e
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-02-06 16:00:17 +0200

    Specs.

--- spec/hex/token_spec.rb
+++ spec/hex/token_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Hex::Token do
   it "generate" do
     expect(Tokenized.new.hex_token).not_to eq nil
   end
+
+  it "generate uniq" do
+    obj = Tokenized.new
+    t1 = obj.hex_token
+    t2 = obj.hex_token
+    expect(t2).not_to eq t1
+  end
 end
 
 class Tokenized
